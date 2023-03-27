e06c322d752ed7f6076acbe87e0c835e7650fc0a
Jonemanuel <jonatasemanuel276@gmail.com>
2023-03-27 01:10:05 -0300

    Projeto SmarTV

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        int a,b;
+        a = 6;
+        b = 6;
+
+        String resultado = a==b? "Verdadeiro":"Falso";
+        
+        System.out.println(resultado);
+
+    }
+}
